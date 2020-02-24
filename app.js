b153d8c39720a477e6c377bc7ed00ec02dc3e46d
Tushar Kanjariya <kanjariyaram2017@gmail.com>
2020-02-24 17:41:03 +0530

    Inital Backend

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const multer = require("multer");
+const cors = require("cors");
+
+var app = express();
+app.use(cors());
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./images/uploads");
+  },
+  filename: (req, file, cb) => {
+    let ext = file.originalname.substring(
+      file.originalname.lastIndexOf("."),
+      file.originalname.length
+    );
+    cb(null, file.fieldname + Date.now() + ext);
+  }
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: function(req, file, cb) {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      return cb(new Error("Supported Files are JPG, PNG and JPEG Formats."));
+    }
+  }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/images/uploads");
+});
+var postImage = upload.single("image");
+
+app.post("/image", (req, res) => {
+  postImage(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      console.log(err);
+      res.send(err);
+    } else if (err) {
+      console.log(err);
+      res.send(err);
+    } else {
+      res.send("done");
+    }
+  });
+});
+
+app.listen(3000 || process.env.PORT);
